a129ad844fac4757d354af1cc51072aa12260192
真高兴 <shuaibbttf4444@163.com>
2015-06-09 19:40:59 +0800

    IOS 7 开始 废除了UDID，使用UUID

--- IOS_UUID/ViewController.m
+++ IOS_UUID/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by Maculish Ting on 15/6/9.
 //  Copyright (c) 2015年 LYD. All rights reserved.
-//
+//  @" http://zengrong.net/post/2152.htm"
 
 #import "ViewController.h"
 
@@ -16,12 +16,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    for (int a=10; a<20; a++) {
+        [self uuID];
+    }
+    [self uuID];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)uuID
+{
+    NSString *retrieveuuid = [[NSString alloc]init];
+    if ( retrieveuuid == nil || [retrieveuuid isEqualToString:@""])
+    {
+        CFUUIDRef uuid = CFUUIDCreate(NULL);
+        assert(uuid != NULL);
+        CFStringRef uuidStr = CFUUIDCreateString(NULL, uuid);
+        retrieveuuid = [NSString stringWithFormat:@"%@", uuidStr];
+        NSLog(@"%@",retrieveuuid);
+    }
+
 }
 
 @end

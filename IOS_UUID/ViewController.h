dc54682ff08c8e285c2e61332a1776e400f816e9
真高兴 <shuaibbttf4444@163.com>
2015-06-09 19:01:13 +0800

    Initial Commit

---
+++ IOS_UUID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS_UUID
+//
+//  Created by Maculish Ting on 15/6/9.
+//  Copyright (c) 2015年 LYD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IOS_UUID/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IOS_UUID
+//
+//  Created by Maculish Ting on 15/6/9.
+//  Copyright (c) 2015年 LYD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
